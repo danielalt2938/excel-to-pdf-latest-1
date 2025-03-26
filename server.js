70bd5859be26550a350f17b4485e850d0ff24f37
danielalt2938 <166267537+danielalt2938@users.noreply.github.com>
2025-03-26 05:53:57 +0000

    udpated smth

--- server.js
+++ server.js
@@ -112,6 +112,10 @@ app.post("/generate-pdf", async (req, res) => {
   }
 });
 
+app.get("/", (req, res) => {
+  res.send("Hello, World!");
+});
+
 // Start Server
 const PORT = process.env.PORT || 5000;
 app.listen(PORT, () => console.log(`✅ Server running on port ${PORT}`));
